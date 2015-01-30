eed7559564dc2e484944c159cf5cff49a564bdc8
Cody McDonald <cody@zenplanner.com>
2015-01-30 15:54:42 -0700

    Initial Commit

---
+++ WorkoutTracker2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorkoutTracker2
+//
+//  Created by Cody McDonald on 1/30/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorkoutTracker2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorkoutTracker2
+//
+//  Created by Cody McDonald on 1/30/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
