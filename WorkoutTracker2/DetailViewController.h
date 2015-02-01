c97fd6184878473b1f832ed19923da25db4c6ba3
Cody McDonald <cody@zenplanner.com>
2015-01-31 17:31:31 -0700

    2nd View Recreated - one way segue working

--- WorkoutTracker2/DetailViewController.h
+++ WorkoutTracker2/DetailViewController.h
@@ -2,12 +2,12 @@
 //  DetailViewController.h
 //  WorkoutTracker2
 //
-//  Created by Cody McDonald on 1/29/15.
+//  Created by Cody McDonald on 1/31/15.
 //  Copyright (c) 2015 msse650. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface DetailViewController : NSObject
+@interface DetailViewController : UIViewController
 
 @end
--- WorkoutTracker2/DetailViewController.m
+++ WorkoutTracker2/DetailViewController.m
@@ -2,12 +2,36 @@
 //  DetailViewController.m
 //  WorkoutTracker2
 //
-//  Created by Cody McDonald on 1/29/15.
+//  Created by Cody McDonald on 1/31/15.
 //  Copyright (c) 2015 msse650. All rights reserved.
 //
 
 #import "DetailViewController.h"
 
+@interface DetailViewController ()
+
+@end
+
 @implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
